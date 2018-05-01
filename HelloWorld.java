4f2f1b169c1b055e031d640d6b724583c260c9b4
unknown <Dao@ASUS-Lite.com>
2018-04-30 19:16:52 -0700

    added printHowAreYou method

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,4 +4,7 @@ public class HelloWorld {
     public void printHelloWorld(){
         System.out.println("Hello World");
     }
+    public void printHowAreYou(){
+        System.out.println("How are you?");
+    }
 }
